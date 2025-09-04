ac9db4894f98842aff593437eaa3540d21dc518c
Italo Santos <italofsan@outlook.com>
2025-09-04 20:18:57 -0300

    Adiciona componente Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,8 @@ import { View } from "react-native";
 import { HomeHeader } from "@/components/HomeHeader";
 import { Target } from "@/components/Target";
 import { List } from "@/components/List";
+import { Button } from "@/components/Button";
+import { router } from "expo-router";
 
 const summary = {
   total: "R$ 2.680,00",
@@ -40,12 +42,21 @@ export default function Index() {
 
       <List
         title="Metas"
-        data={[]}
+        data={targets}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhuma meta. Toque em nova meta para criar."
         containerStyle={{ paddingHorizontal: 24 }}
       />
+
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   );
 }
