298b837df7bfb146581499c32039c33301025f94
Italo Santos <italofsan@outlook.com>
2025-09-04 20:04:32 -0300

    Adiciona componente Target

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,29 @@
 import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
+
+const summary = {
+  total: "R$ 2.680,00",
+  input: { label: "Entradas", value: "R$ 6.184,90" },
+  output: { label: "Saídas", value: "-R$ 883,65" },
+};
+
+const targets = [
+  {
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "900,00",
+    target: "1.200,00",
+  },
+];
 
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
-      <HomeHeader data={{ total: "R$ 2.680,00" }} />
+      <HomeHeader data={summary} />
+
+      <Target data={targets[0]} />
     </View>
   );
 }
--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -4,12 +4,14 @@ import { LinearGradient } from "expo-linear-gradient";
 import { colors } from "@/theme/colors";
 
 import { Separator } from "../Separator";
-import { Summary } from "../Summary";
+import { Summary, SummaryProps } from "../Summary";
 
 import { styles } from "./styles";
 
 export type HomeHeaderProps = {
   total: string;
+  input: SummaryProps;
+  output: SummaryProps;
 };
 
 type Props = {
@@ -31,13 +33,13 @@ export function HomeHeader({ data }: Props) {
 
       <View style={styles.summary}>
         <Summary
-          data={{ label: "Entradas", value: "R$ 6.184,90" }}
+          data={data.input}
           icon={{ name: "arrow-upward", color: colors.green[500] }}
         />
 
         <Summary
           isLeft
-          data={{ label: "Saídas", value: "-R$ 883,65" }}
+          data={data.output}
           icon={{ name: "arrow-downward", color: colors.red[400] }}
         />
       </View>
