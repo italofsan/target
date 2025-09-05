e7c3df0fb40e2e9f07dc1afdb5871ff9d1d2a637
Italo Santos <italofsan@outlook.com>
2025-09-04 23:05:32 -0300

    Adiciona componente Progress

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,12 +1,29 @@
 import { router, useLocalSearchParams } from "expo-router";
 import { Button, Text, View } from "react-native";
 
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+
+const details = {
+  current: "R$ 580,00",
+  target: "R$ 1.780,00",
+  percentage: 25,
+};
+
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>ID: {params.id}</Text>
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButton={{
+          icon: "edit",
+          onPress: () => {},
+        }}
+      />
+
+      <Progress data={details} />
 
       <Button title="Voltar" onPress={() => router.back()} />
     </View>
