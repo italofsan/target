2a544f871fcb98ffcd52457a93f7f33d32c4f550
Italo Santos <italofsan@outlook.com>
2025-09-05 19:41:05 -0300

    Finaliza tela de Nova Transação

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import { router, useLocalSearchParams } from 'expo-router'
 import { Text, View } from 'react-native'
 
+import { TransactionTypes } from '@/utils/TransactionTypes'
+
+import { TransactionType } from '@/components/TransactionTypes'
 import { CurrencyInput } from '@/components/CurrencyInput'
 import { PageHeader } from '@/components/PageHeader'
 import { Button } from '@/components/Button'
@@ -8,6 +12,7 @@ import { Input } from '@/components/Input'
 
 export default function Transaction() {
   const params = useLocalSearchParams<{ id: string }>()
+  const [type, setType] = useState(TransactionTypes.Input)
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
@@ -17,13 +22,12 @@ export default function Transaction() {
       />
 
       <View style={{ marginTop: 32, gap: 24 }}>
+        <TransactionType selected={type} onChange={setType} />
         <CurrencyInput label='Valor (R$)' value={0} />
-
         <Input
           label='Motivo'
           placeholder='Ex: Investir em CDB de 110% no banco XPTO'
         />
-
         <Button title='Salvar' />
       </View>
     </View>
