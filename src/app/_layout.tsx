0e768a5c10c36d313295779bd232ddbc3f5e2d32
Italo Santos <italofsan@outlook.com>
2025-09-05 21:22:39 -0300

    Criando conexão com o banco de dados

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,33 +1,41 @@
-import { Stack } from "expo-router";
+import { Suspense } from 'react'
+import { Stack } from 'expo-router'
+import { SQLiteProvider } from 'expo-sqlite'
 
 import {
   useFonts,
   Inter_400Regular,
   Inter_500Medium,
   Inter_700Bold,
-} from "@expo-google-fonts/inter";
+} from '@expo-google-fonts/inter'
 
-import { Loading } from "@/components/Loading";
+import { migrate } from '@/database/migrate'
 
-import { colors } from "@/theme/colors";
+import { Loading } from '@/components/Loading'
+
+import { colors } from '@/theme/colors'
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_500Medium,
     Inter_700Bold,
-  });
+  })
 
   if (!fontsLoaded) {
-    return <Loading />;
+    return <Loading />
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.white },
-      }}
-    />
-  );
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName='target.db' onInit={migrate} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: colors.white },
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
+  )
 }
