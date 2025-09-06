f70f370e06409cac61e5a8e0c68ab724012e22f2
Italo Santos <italofsan@outlook.com>
2025-09-05 23:01:39 -0300

    Adiciona método de listagem de metas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,48 +1,69 @@
-import { router } from "expo-router";
-import { StatusBar, View } from "react-native";
+import { useCallback } from 'react'
+import { router, useFocusEffect } from 'expo-router'
+import { Alert, StatusBar, View } from 'react-native'
 
-import { HomeHeader } from "@/components/HomeHeader";
-import { Target } from "@/components/Target";
-import { Button } from "@/components/Button";
-import { List } from "@/components/List";
+import { useTargetDatabase } from '@/database/useTargetDatabase'
+
+import { HomeHeader } from '@/components/HomeHeader'
+import { Target } from '@/components/Target'
+import { Button } from '@/components/Button'
+import { List } from '@/components/List'
 
 const summary = {
-  total: "R$ 2.680,00",
-  input: { label: "Entradas", value: "R$ 6.184,90" },
-  output: { label: "Saídas", value: "-R$ 883,65" },
-};
+  total: 'R$ 2.680,00',
+  input: { label: 'Entradas', value: 'R$ 6.184,90' },
+  output: { label: 'Saídas', value: '-R$ 883,65' },
+}
 
 const targets = [
   {
-    id: "1",
-    name: "Apple Watch",
-    percentage: "50%",
-    current: "R$ 580,00",
-    target: "R$ 1.790,00",
+    id: '1',
+    name: 'Apple Watch',
+    percentage: '50%',
+    current: 'R$ 580,00',
+    target: 'R$ 1.790,00',
   },
   {
-    id: "2",
-    name: "Comprar uma cadeira ergonômica",
-    percentage: "75%",
-    current: "R$ 900,00",
-    target: "R$ 1.200,00",
+    id: '2',
+    name: 'Comprar uma cadeira ergonômica',
+    percentage: '75%',
+    current: 'R$ 900,00',
+    target: 'R$ 1.200,00',
   },
   {
-    id: "3",
-    name: "Comprar uma cadeira ergonômica",
-    percentage: "75%",
-    current: "R$ 1.200,00",
-    target: "R$ 3.000,00",
+    id: '3',
+    name: 'Comprar uma cadeira ergonômica',
+    percentage: '75%',
+    current: 'R$ 1.200,00',
+    target: 'R$ 3.000,00',
   },
-];
+]
 export default function Index() {
+  const targetDatabase = useTargetDatabase()
+
+  async function fetchTargets() {
+    try {
+      const response = await targetDatabase.listBySavedValue()
+      console.log(response)
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível carregar as metas.')
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchTargets()
+    }, [])
+  )
+
   return (
     <View style={{ flex: 1 }}>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle='light-content' />
       <HomeHeader data={summary} />
 
       <List
-        title="Metas"
+        title='Metas'
         data={targets}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
@@ -51,13 +72,13 @@ export default function Index() {
             onPress={() => router.navigate(`/in-progress/${item.id}`)}
           />
         )}
-        emptyMessage="Nenhuma meta. Toque em nova meta para criar."
+        emptyMessage='Nenhuma meta. Toque em nova meta para criar.'
         containerStyle={{ paddingHorizontal: 24 }}
       />
 
       <View style={{ padding: 24, paddingBottom: 32 }}>
-        <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+        <Button title='Nova meta' onPress={() => router.navigate('/target')} />
       </View>
     </View>
-  );
+  )
 }
